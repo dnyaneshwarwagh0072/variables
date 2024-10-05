3e0970e4e6331f02176399d9d745b892db985fec
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-05 19:32:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,187 @@
+// console.log("MyAge");
+// console.log(MyAge);
+// var MyAge = 33;
+// console.log(MyAge);
+
+
+// console.log("Hello, World");
+// console.log("Hello, JavaScript");
+
+
+// var a=10;
+// console.log("My num is ", a);
+
+
+// var a=10.45;
+// a=20.55;
+// console.log(a);
+
+
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+
+// var a = 1042;
+// var b = 7;
+// var quo = a / b;
+// var rem = a % b;
+// console.log(quo, rem);
+
+
+// var input = 8;
+// console.log("8 * 1 =", 8 * 1);
+// console.log("8 * 2 =", 8 * 2);
+// console.log("8 * 3 =", 8 * 3);
+// console.log("8 * 4 =", 8 * 4);
+// console.log("8 * 5 =", 8 * 5);
+// console.log("8 * 6 =", 8 * 6);
+// console.log("8 * 7 =", 8 * 7);
+// console.log("8 * 8 =", 8 * 8);
+// console.log("8 * 9 =", 8 * 9);
+// console.log("8 * 10 =", 8 * 10);
+
+
+// var a=8;
+// var b=3;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(Math.round(a/b));
+// console.log(Math.floor(a/b));
+// console.log(a%b);
+
+
+
+
+
+
+
+// Let..Var..Const...
+
+// var age = 20;
+// var age = 21;
+// console.log(age);
+
+
+// let year = 10;
+// for (let i = 1; i < year; i++) {
+//     console.log(i);
+// }
+// console.log(year);
+
+
+// const year = 10;
+// for (const i = 1; i < year; i++) {
+//     console.log(i);
+// }
+// console.log(year);
+
+
+
+
+
+
+
+
+
+// Hoisting...
+
+// console.log(age); 
+// var age = 33;
+
+
+
+
+
+
+
+
+// let age = 20;
+// console.log(age);
+// age = 21;
+// console.log(age);
+
+
+
+
+
+// const age = 20;
+// age = 21;
+// console.log(age);
+
+
+
+
+
+
+
+
+
+
+// SCOPE
+
+// {
+//     {
+//         var age = 20;
+//     }
+// }
+// console.log(age);
+
+
+// function myAge() {
+//     var newAge = 30;
+//     console.log (age);
+// }
+// console.log(newAge);
+
+
+
+
+
+
+
+
+// const hello = (n) => {
+//     console.log("Hello JavaScript this is", n);
+// }
+// hello(2024);
+
+
+
+
+
+
+
+
+// let day = "tue";
+// switch (day) {
+//     case "mon":
+//         console.log("Moday");
+//         break;
+//     case "tue":
+//         console.log("Tuesday");
+//         break;
+//     case "wed":
+//         console.log("Wednesday");
+//         break;
+// }
+
+
+
+
+
+
+
+
+// let age = 29;
+// console.log(age <= 18 ? "Go to College" : age > 18 && age <= 21 ? "Go to Degree" : "Go to Interview");
+
+
+
+
+
+
+
+
